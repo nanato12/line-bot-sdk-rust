ad9a0d6aed579314a7ace1e017c2d229170f14e1
nanato12 <admin@nanato12.info>
2021-02-24 03:09:41 +0900

    Update member_leave.rs

--- src/events/member_leave.rs
+++ src/events/member_leave.rs
@@ -1,13 +1,17 @@
+use crate::events::Member;
+use crate::events::Source;
+
 use serde_derive::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct MemberLeaveEvent {
+    pub mode: String,
+    pub timestamp: i64,
+    pub source: Source,
+    pub left: Left,
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Left {
     pub members: Vec<Member>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Member {
-    pub r#type: String,
-    #[serde(rename = "userId")]
-    pub user_id: String,
 }
