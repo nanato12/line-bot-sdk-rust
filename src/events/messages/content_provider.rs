81e19f613af1be8509874660fa9c0c9eba740121
nanato12 <admin@nanato12.info>
2021-02-24 04:31:01 +0900

    Update content_provider.rs

--- src/events/messages/content_provider.rs
+++ src/events/messages/content_provider.rs
@@ -2,9 +2,20 @@ use serde_derive::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct ContentProvider {
-    pub r#type: String,
-    #[serde(rename = "originalContentUrl")]
-    pub original_content_url: Option<String>,
-    #[serde(rename = "previewImageUrl")]
-    pub preview_image_url: Option<String>,
+    #[serde(flatten)]
+    pub r#type: ContentProviderType,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+pub enum ContentProviderType {
+    #[serde(rename = "external")]
+    External {
+        #[serde(rename = "originalContentUrl")]
+        original_content_url: String,
+        #[serde(rename = "previewImageUrl")]
+        preview_image_url: String,
+    },
+    #[serde(other)]
+    Other,
 }
