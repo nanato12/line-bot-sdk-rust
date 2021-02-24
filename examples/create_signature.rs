882cf0440c48c602fc9422900a41dab309f11b90
nanato12 <admin@nanato12.info>
2021-02-25 02:02:07 +0900

    Add create_signature.rs

---
+++ examples/create_signature.rs
@@ -0,0 +1,58 @@
+extern crate line_bot_sdk_rust as line;
+
+use base64::encode;
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+
+// Signature confirmation
+pub fn create_signature(channel_secret: &str, body: &str) -> String {
+    type HmacSha256 = Hmac<Sha256>;
+
+    let mut mac =
+        HmacSha256::new_varkey(channel_secret.as_bytes()).expect("HMAC can take key of any size");
+    mac.input(body.as_bytes());
+    return encode(&mac.result().code().to_vec());
+}
+
+fn main() {
+    let request_body: &str = r#"
+          {
+            "destination": "xxxxxxxxxx",
+            "events": [
+              {
+                "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                "type": "message",
+                "mode": "active",
+                "timestamp": 1462629479859,
+                "source": {
+                  "type": "user",
+                  "userId": "U4af4980629..."
+                },
+                "message": {
+                  "id": "325708",
+                  "type": "text",
+                  "text": "@example Hello, world! (love)",
+                  "emojis": [
+                    {
+                      "index": 14,
+                      "length": 6,
+                      "productId": "5ac1bfd5040ab15980c9b435",
+                      "emojiId": "001"
+                    }
+                  ],
+                  "mention": {
+                    "mentionees": [
+                      {
+                        "index": 0,
+                        "length": 8,
+                        "userId": "U850014438e..."
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        "#;
+    println!("{}", create_signature("channel_secret", request_body));
+}
