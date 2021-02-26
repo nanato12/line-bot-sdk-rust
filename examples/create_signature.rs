60288e168f05968e5ebb9362ade18827f42d92ef
nanato12 <admin@nanato12.info>
2021-02-27 03:38:16 +0900

    Update create_signature.rs

--- examples/create_signature.rs
+++ examples/create_signature.rs
@@ -1,3 +1,7 @@
+extern crate line_bot_sdk_rust as line;
+
+use line::webhook::validate_signature;
+
 use base64::encode;
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
@@ -13,44 +17,51 @@ pub fn create_signature(channel_secret: &str, body: &str) -> String {
 }
 
 fn main() {
+    let channel_secret: &str = "channel_secret";
     let request_body: &str = r#"
-          {
+        {
             "destination": "xxxxxxxxxx",
             "events": [
-              {
-                "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-                "type": "message",
-                "mode": "active",
-                "timestamp": 1462629479859,
-                "source": {
-                  "type": "user",
-                  "userId": "U4af4980629..."
-                },
-                "message": {
-                  "id": "325708",
-                  "type": "text",
-                  "text": "@example Hello, world! (love)",
-                  "emojis": [
-                    {
-                      "index": 14,
-                      "length": 6,
-                      "productId": "5ac1bfd5040ab15980c9b435",
-                      "emojiId": "001"
+                {
+                    "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                    "type": "message",
+                    "mode": "active",
+                    "timestamp": 1462629479859,
+                    "source": {
+                        "type": "user",
+                        "userId": "U4af4980629..."
+                    },
+                    "message": {
+                        "id": "325708",
+                        "type": "text",
+                        "text": "@example Hello, world! (love)",
+                        "emojis": [
+                                {
+                                    "index": 14,
+                                    "length": 6,
+                                    "productId": "5ac1bfd5040ab15980c9b435",
+                                    "emojiId": "001"
+                                }
+                            ],
+                        "mention": {
+                            "mentionees": [
+                                {
+                                    "index": 0,
+                                    "length": 8,
+                                    "userId": "U850014438e..."
+                                }
+                            ]
+                        }
                     }
-                  ],
-                  "mention": {
-                    "mentionees": [
-                      {
-                        "index": 0,
-                        "length": 8,
-                        "userId": "U850014438e..."
-                      }
-                    ]
-                  }
                 }
-              }
             ]
-          }
-        "#;
-    println!("{}", create_signature("channel_secret", request_body));
+        }
+    "#;
+    let signature: String = create_signature(channel_secret, request_body);
+
+    if validate_signature(channel_secret, &signature, request_body) {
+        println!("Success: {}", signature);
+    } else {
+        println!("NG");
+    }
 }
