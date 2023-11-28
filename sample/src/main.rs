2c933f9af946ee774fdee48860cdd77bba2a9fde
nanato12 <admin@okj.info>
2023-11-28 14:21:00 +0900

    update: sample

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+use line_bot_sdk_rust_openapi::messaging_api::{
+    apis::{
+        configuration::Configuration,
+        messaging_api_api::{reply_message, ReplyMessageParams},
+    },
+    models::{Message, ReplyMessageRequest, TextMessage},
+};
+
+#[tokio::main]
+async fn main() {
+    let text_message = TextMessage::new("text".to_string(), "message".to_string());
+    let params = ReplyMessageParams {
+        reply_message_request: ReplyMessageRequest {
+            reply_token: "a".to_string(),
+            messages: vec![Message::Text(text_message)],
+            notification_disabled: Some(false),
+        },
+    };
+    let result = reply_message(&Configuration::default(), params).await;
+    match result {
+        Ok(r) => println!("{:#?}", r),
+        Err(e) => println!("{:#?}", e),
+    }
     println!("Hello, world!");
 }
