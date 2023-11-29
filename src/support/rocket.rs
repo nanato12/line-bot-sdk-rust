b1d9a52254c7b18b38786992b75b70399ab82e44
nanato12 <admin@okj.info>
2023-11-30 02:18:48 +0900

    add: rocket support

--- src/support/rocket.rs
+++ src/support/rocket.rs
@@ -1,46 +1,45 @@
-//! Support for `rocket` framework
-//! # Example
-//! ```
-//! #![feature(proc_macro_hygiene, decl_macro)]
-//!
-//! #[macro_use]
-//! extern crate rocket;
-//!
-//! extern crate line_bot_sdk_rust as line;
-//! use line::support::rocket_support::{Body, Signature};
-//!
-//! #[post("/callback", data = "<body>")]
-//! fn callback(signature: Signature, body: Body) {
-//!     // more
-//! }
-//!
-//! fn main() {
-//!     rocket::ignite().mount("/", routes![callback]).launch();
-//! }
-//! ```
-use rocket::data::{self, FromDataSimple};
-use rocket::http::{ContentType, Status};
-use rocket::request::{self, FromRequest};
-use rocket::Outcome::{Failure, Forward, Success};
-use rocket::{Data, Request};
-use std::io::Read;
+/*
+* Copyright 2023 nanato12
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
-/// Signature in HTTP Request headers
+use rocket::{
+    data::FromData,
+    http::{ContentType, Status},
+    request::{FromRequest, Outcome, Request},
+    Data,
+};
+
 #[derive(Debug)]
 pub struct Signature {
     pub key: String,
 }
 
-impl<'a, 'r> FromRequest<'a, 'r> for Signature {
-    type Error = ();
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let keys: Vec<_> = request.headers().get("X-Line-Signature").collect();
-        match keys.len() {
-            0 => Failure((Status::BadRequest, ())),
-            1 => Success(Signature {
-                key: keys[0].to_string(),
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Signature {
+    type Error = String;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let token = request.headers().get_one("x-line-signature");
+        match token {
+            Some(token) => Outcome::Success(Signature {
+                key: token.to_string(),
             }),
-            _ => Failure((Status::BadRequest, ())),
+            None => Outcome::Error((
+                Status::BadRequest,
+                "x-line-signature is missing".to_string(),
+            )),
         }
     }
 }
@@ -51,20 +50,26 @@ pub struct Body {
     pub string: String,
 }
 
-impl FromDataSimple for Body {
-    type Error = String;
-    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
-        // ContentType must be application/json
-        let body_ct = ContentType::new("application", "json");
-        if req.content_type() != Some(&body_ct) {
-            return Forward(data);
-        }
-        // Parse data to String
-        let mut string = String::new();
-        if let Err(e) = data.open().read_to_string(&mut string) {
-            return Failure((Status::InternalServerError, format!("{:?}", e)));
-        }
-        // Return successfully.
-        Success(Body { string })
-    }
-}
+// #[rocket::async_trait]
+// impl<'r> FromData<'r> for Body {
+//     type Error = String;
+
+//     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<Self, Self::Error> {
+//         // ContentType must be application/json
+//         let body_ct = ContentType::new("application", "json");
+//         if req.content_type() != Some(&body_ct) {
+//             return Outcome::Forward(data);
+//         }
+//         let limit = req.limits().get("person").unwrap_or(256_i32.bytes());
+
+//         // Parse data to String
+//         let mut string = data.open(limit).into_string().await.unwrap();
+//         if !string.is_complete() {
+//             return Outcome::Error((Status::InternalServerError, "ng".to_string()));
+//         }
+//         // Return successfully.
+//         Outcome::Success(Body {
+//             string: string.into_inner(),
+//         })
+//     }
+// }
