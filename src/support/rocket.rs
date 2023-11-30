6d3e430fa704772d3800390a37d546cf1fa59979
nanato12 <admin@okj.info>
2023-11-30 10:51:55 +0900

    update: rocket supprt

--- src/support/rocket.rs
+++ src/support/rocket.rs
@@ -15,10 +15,8 @@
 */
 
 use rocket::{
-    data::FromData,
-    http::{ContentType, Status},
+    http::Status,
     request::{FromRequest, Outcome, Request},
-    Data,
 };
 
 #[derive(Debug)]
@@ -43,33 +41,3 @@ impl<'r> FromRequest<'r> for Signature {
         }
     }
 }
-
-/// HTTP Request body
-#[derive(Debug)]
-pub struct Body {
-    pub string: String,
-}
-
-// #[rocket::async_trait]
-// impl<'r> FromData<'r> for Body {
-//     type Error = String;
-
-//     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<Self, Self::Error> {
-//         // ContentType must be application/json
-//         let body_ct = ContentType::new("application", "json");
-//         if req.content_type() != Some(&body_ct) {
-//             return Outcome::Forward(data);
-//         }
-//         let limit = req.limits().get("person").unwrap_or(256_i32.bytes());
-
-//         // Parse data to String
-//         let mut string = data.open(limit).into_string().await.unwrap();
-//         if !string.is_complete() {
-//             return Outcome::Error((Status::InternalServerError, "ng".to_string()));
-//         }
-//         // Return successfully.
-//         Outcome::Success(Body {
-//             string: string.into_inner(),
-//         })
-//     }
-// }
