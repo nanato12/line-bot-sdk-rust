56539f52b22f7acbc5fbdbefbad8d485a139a6f6
nanato12 <admin@okj.info>
2023-12-02 02:11:27 +0900

    update: generator

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -24,7 +24,7 @@ const LICENSE: &str = r#"/*
 
 const OPENAPI_GENERATOR_CLI_VERSION: &str = "7.1.0";
 const SPEC_DIR: &str = "line-openapi";
-const OUTPUT_DIR: &str = "openapi/src";
+const OUTPUT_DIR: &str = "core";
 
 // replace_in_file - Use hashmap to replace the entire contents of a file.
 fn replace_in_file(file_path: &Path, replacements: HashMap<&str, &str>) {
@@ -40,23 +40,23 @@ fn replace_in_file(file_path: &Path, replacements: HashMap<&str, &str>) {
     file.write_all(contents.as_bytes()).unwrap();
 }
 
-fn fix_openapi_messaging_api(file_path: &Path) {
-    let mut replacements: HashMap<&str, &str> = HashMap::new();
-    let p = file_path.to_str().unwrap();
+// fn fix_openapi_messaging_api(file_path: &Path) {
+//     let mut replacements: HashMap<&str, &str> = HashMap::new();
+//     let p = file_path.to_str().unwrap();
 
-    // delete type from event, source, message_content
-    if p.contains("_message.rs") {
-        replacements.insert("#[serde(rename = \"type\")]", "");
-        replacements.insert("pub r#type: String,", "");
-        replacements.insert("r#type,", "");
-        replacements.insert("pub fn new(r#type: String, ", "pub fn new(");
-        if p.contains("_message.rs") {
-            replacements.insert("/// Type of message", "");
-        }
-    }
+//     // delete type from event, source, message_content
+//     if p.contains("_message.rs") {
+//         replacements.insert("#[serde(rename = \"type\")]", "");
+//         replacements.insert("pub r#type: String,", "");
+//         replacements.insert("r#type,", "");
+//         replacements.insert("pub fn new(r#type: String, ", "pub fn new(");
+//         if p.contains("_message.rs") {
+//             replacements.insert("/// Type of message", "");
+//         }
+//     }
 
-    replace_in_file(file_path, replacements);
-}
+//     replace_in_file(file_path, replacements);
+// }
 
 fn fix_openapi_webhook(file_path: &Path) {
     let mut replacements: HashMap<&str, &str> = HashMap::new();
@@ -81,26 +81,29 @@ fn fix_openapi_webhook(file_path: &Path) {
     replace_in_file(file_path, replacements);
 }
 
-fn fix_openapi(file_path: &Path, pkg_name: &str) {
-    let rep_model = &format!("crate::{pkg_name}::models");
-    let rep_api = &format!("crate::{pkg_name}::apis");
-
+fn fix_openapi_manage_audience(file_path: &Path) {
     let replacements: HashMap<&str, &str> = [
         (
             "status: Option<AudienceGroupStatus>",
-            "status: Option<crate::manage_audience::models::AudienceGroupStatus>",
+            "status: Option<crate::models::AudienceGroupStatus>",
         ),
         (
             "create_route: Option<AudienceGroupCreateRoute>",
-            "create_route: Option<crate::manage_audience::models::AudienceGroupCreateRoute>",
+            "create_route: Option<crate::models::AudienceGroupCreateRoute>",
         ),
-        (
-            "pub enum AreaDemographic",
-            "#[allow(non_camel_case_types)]\npub enum AreaDemographic",
-        ),
-        ("crate::models", rep_model),
-        ("crate::apis", rep_api),
     ]
+    .iter()
+    .cloned()
+    .collect();
+
+    replace_in_file(file_path, replacements);
+}
+
+fn fix_openapi_messaging_api(file_path: &Path) {
+    let replacements: HashMap<&str, &str> = [(
+        "pub enum AreaDemographic",
+        "#[allow(non_camel_case_types)]\npub enum AreaDemographic",
+    )]
     .iter()
     .cloned()
     .collect();
@@ -118,12 +121,26 @@ fn process_directory(dir_path: &PathBuf, pkg_name: &str) {
                 } else if let Some(extension) = path.extension() {
                     if extension == "rs" {
                         println!("{}", path.as_path().to_str().unwrap());
-                        fix_openapi(path.as_path(), pkg_name);
 
-                        if path.to_str().unwrap().contains("src/webhook/") {
-                            fix_openapi_webhook(path.as_path());
+                        if path
+                            .to_str()
+                            .unwrap()
+                            .contains(&format!("{OUTPUT_DIR}/manage_audience/"))
+                        {
+                            fix_openapi_manage_audience(path.as_path());
                         }
-                        if path.to_str().unwrap().contains("src/messaging_api/") {
+                        if path
+                            .to_str()
+                            .unwrap()
+                            .contains(&format!("{OUTPUT_DIR}/webhook/"))
+                        {
+                            // fix_openapi_webhook(path.as_path());
+                        }
+                        if path
+                            .to_str()
+                            .unwrap()
+                            .contains(&format!("{OUTPUT_DIR}/messaging_api/"))
+                        {
                             fix_openapi_messaging_api(path.as_path());
                         }
 
@@ -157,11 +174,6 @@ fn main() {
             .status();
     }
 
-    let _ = fs::copy(
-        "./tools/.openapi-generator-ignore",
-        format!("{OUTPUT_DIR}/.openapi-generator-ignore"),
-    );
-
     let services = vec![
         "channel-access-token",
         "insight",
@@ -176,18 +188,20 @@ fn main() {
 
     for service in services {
         let pkg_name = &service.replace("-", "_");
-        let pkg_dir_path = &format!("{OUTPUT_DIR}/{pkg_name}",);
+        let pkg_dir = &format!("{OUTPUT_DIR}/{pkg_name}");
+        let lib_pkg_dir = &format!("{OUTPUT_DIR}/lib/src/{pkg_name}");
 
-        let remove_existing_pkg_file = Command::new("rm").arg("-rf").arg(pkg_dir_path).status();
-        match remove_existing_pkg_file {
-            Ok(status) => {
-                if status.code() != Some(0) {
-                    continue;
-                }
-            }
-            Err(_) => {}
-        }
+        // Initialize package directory
+        let _ = Command::new("rm").arg("-rf").arg(pkg_dir).status();
+        let _ = Command::new("mkdir").arg("-p").arg(pkg_dir).status();
 
+        // Place .openapi-generator-ignore in the package directory
+        let _ = fs::copy(
+            "./tools/.openapi-generator-ignore",
+            format!("{pkg_dir}/.openapi-generator-ignore"),
+        );
+
+        // Run openapi-generator-cli
         let openapi_generate_result = Command::new("java")
             .arg("-Dlog.level=error")
             .arg("-jar")
@@ -204,11 +218,7 @@ fn main() {
             .arg("-g")
             .arg("rust")
             .arg("-o")
-            .arg(OUTPUT_DIR)
-            // .arg("--additional-properties")
-            // .arg(format!("enumNameSuffix={}", pkg_name.to_case(Case::Pascal)))
-            // .arg("--additional-properties")
-            // .arg("supportMiddleware=true")
+            .arg(pkg_dir)
             .arg("--additional-properties")
             .arg("supportMultipleResponses=true")
             .arg("--additional-properties")
@@ -221,38 +231,27 @@ fn main() {
             continue;
         }
 
-        let _ = Command::new("mv")
-            .arg(format!("{OUTPUT_DIR}/src"))
-            .arg(pkg_dir_path)
-            .status();
-
-        let _ = fs::copy("./tools/sources/mod.rs", format!("{pkg_dir_path}/mod.rs"));
-
-        process_directory(&PathBuf::from(pkg_dir_path), pkg_name)
+        process_directory(&PathBuf::from(pkg_dir), pkg_name)
     }
 
-    let sources = vec![
-        "messaging_api/models/message.rs",
-        "webhook/models/message_content.rs",
-        "webhook/models/event.rs",
-        "webhook/models/source.rs",
-    ];
+    // let sources = vec![
+    //     "messaging_api/models/message.rs",
+    //     "webhook/models/message_content.rs",
+    //     "webhook/models/event.rs",
+    //     "webhook/models/source.rs",
+    // ];
 
-    for source in sources {
-        let _ = fs::copy(
-            format!("./tools/sources/{source}"),
-            format!("openapi/src/{source}"),
-        );
-    }
+    // for source in sources {
+    //     let _ = fs::copy(
+    //         format!("./tools/sources/{source}"),
+    //         format!("openapi/src/{source}"),
+    //     );
+    // }
 
     let _ = Command::new("cargo")
         .arg("fix")
         .arg("--allow-dirty")
-        .current_dir("openapi")
         .status();
 
-    let _ = Command::new("cargo")
-        .arg("fmt")
-        .current_dir("openapi")
-        .status();
+    let _ = Command::new("cargo").arg("fmt").status();
 }
